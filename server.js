b7007f3dcbac0694162213a5b6c9f2e4ab717b2e
Poltergeist <brandonssc@gmail.com>
2018-12-14 13:48:46 -0500

    base apollo-express-graphql server

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const { ApolloServer } = require('apollo-server-express');
+
+const typeDefs = require('./api/schema');
+const resolvers = require('./api/resolvers');
+
+const app = express();
+
+const PORT = 4000;
+
+const SERVER = new ApolloServer({
+  typeDefs,
+  resolvers
+});
+
+SERVER.applyMiddleware({ app });
+
+app.get('/', (req, res) => res.send(`Apollo Server Running, access GraphQL playground at http://localhost:${PORT}/graphqln`));
+
+app.listen(PORT, () =>
+  console.log(`🚀 GraphQL playground is running at http://localhost:${PORT}/graphql`)
+);
